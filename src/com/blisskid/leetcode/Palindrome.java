cfaf322bb856343f948d347a49a0194b6fbba7c7
blisskid <yuleirichard@163.com>
2019-08-14 17:29:34 +0800

    ii

--- src/com/blisskid/leetcode/Palindrome.java
+++ src/com/blisskid/leetcode/Palindrome.java
@@ -3,20 +3,22 @@ package com.blisskid.leetcode;
 public class Palindrome {
 
     public static void main(String[] args) {
-
+        Palindrome palindrome = new Palindrome();
+        System.out.println(palindrome.isPalindrome(2134328499));
+        String str = new String("ieiwfkslfls");
     }
 
     public boolean isPalindrome(int x) {
         if (x < 0) {
             return false;
         }
-        int res = 0;
-        while (x > 0) {
-            if (res > 0 && ((Integer.MAX_VALUE - x % 10) / res) < 10) {
+        int res = 0, temp = x;
+        while (temp > 0) {
+            if (res > 0 && ((Integer.MAX_VALUE - temp % 10) / res) < 10) {
                 return false;
             }
-            res = 10 * res + x % 10;
-            x = x / 10;
+            res = 10 * res + temp % 10;
+            temp = temp / 10;
         }
         if (x == res) {
             return true;
