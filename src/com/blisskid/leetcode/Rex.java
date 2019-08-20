ef16a30d52b8f5e5e30aff435247225d77c56139
blisskid <yuleirichard@163.com>
2019-08-21 06:43:11 +0800

    ii

--- src/com/blisskid/leetcode/Rex.java
+++ src/com/blisskid/leetcode/Rex.java
@@ -1,14 +1,15 @@
 package com.blisskid.leetcode;
 
 class Rex {
+    public static void main(String[] args) {
+        Rex rex = new Rex();
+        System.out.println(rex.isMatch("aaa", "ab*a*c*a"));
+    }
+
     public boolean isMatch(String s, String p) {
         //check parameters
         int sl = s.length(), pl = p.length();
-        if (sl == 0 && pl == 0) {
-            return true;
-        } else if (sl == 0 && pl != 0) {
-            return false;
-        } else if (pl == 0 && sl != 0) {
+        if (sl == 0 || pl == 0) {
             return false;
         }
 
@@ -22,28 +23,87 @@ class Rex {
             }
         }
 
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < pl; i++) {
-            if ((p.charAt(i) < 'a' || p.charAt(i) > 'z') && (p.charAt(i) != '*' || p.charAt(i) != '.')) {
+            if ((p.charAt(i) < 'a' || p.charAt(i) > 'z') && p.charAt(i) != '*' && p.charAt(i) != '.') {
                 return false;
             }
+            //remove all the continuous '*', only keep one '*'
+            if (!(i > 0 && p.charAt(i) == '*' && p.charAt(i - 1) == '*')) {
+                sb.append(p.charAt(i));
+            }
         }
+
+        p = sb.toString();
+        pl = p.length();
 
         //compare the two string
         int si = 0, pi = 0;
         while (si < sl && pi < pl) {
-            if (p.charAt(pi) == '.' || p.charAt(pi) == s.charAt(si)) {
+            char cPi = p.charAt(pi);
+            char cSi = s.charAt(si);
+            if (cPi >= 'a' && cPi <= 'z') {
+                if (cSi == cPi) {
+                    si++;
+                    pi++;
+                } else if (pi + 1 < pl && p.charAt(pi + 1) == '*') {
+                    pi++;
+                    pi++;
+                } else {
+                    return false;
+                }
+            } else if (cPi == '.') {
                 si++;
                 pi++;
-            } else if (p.charAt(pi) == '*') {
-                //check if the char in s is the same as last char
-                if (true) {
+            } else if (cPi == '*') {
+                char lastP = p.charAt(pi - 1);
+                if (lastP >= 'a' && lastP <= 'z') {
+                    si--;
+                    pi++;
+                    while (si < sl && pi < pl) {
+                        if (s.charAt(si) == lastP) {
+                            if (p.charAt(pi) == lastP || p.charAt(pi) == '.') {
+                                pi++;
+                                si++;
+                            } else if (pi + 1 < pl && p.charAt(pi + 1) == '*') {
+                                pi = pi + 2;
+                            } else {
+                                si++;
+                            }
 
-                }
-                if (s.charAt(si) == s.charAt(si - 1)) {
-                    si++;
+                        } else {
+                            break;
+                        }
+                    }
+                } else if (lastP == '.') {
+                    if (pi == pl - 1) {
+                        return true;
+                    } else {
+                        String rightLeftPStr = p.substring(pi + 1);
+                        //check if rightLeftPStr match
+                        for (int i = si - 1; i < sl; i++) {
+                            if (isMatch(s.substring(i), rightLeftPStr)) {
+                                return true;
+                            }
+                        }
+                        return false;
+                    }
                 }
             }
         }
-        return true;
+        if (si == sl) {
+            if (pi == pl) {
+                return true;
+            } else {
+                if (pi == pl - 1 && p.charAt(pi) == '*') {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+
+        } else {
+            return false;
+        }
     }
 }
