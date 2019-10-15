94579f5f55b688594a0c3e723b34236ffafda1ff
blisskid <yuleirichard@163.com>
2019-10-15 23:39:13 +0800

    ii

--- src/com/blisskid/leetcode/Solution851.java
+++ src/com/blisskid/leetcode/Solution851.java
@@ -20,27 +20,29 @@ public class Solution851 {
             resultArr[i] = -1;
         }
         for (int i = 0; i < size; i++) {
-            findQuietest(richer, quiet, i);
+            findLoudest(richer, quiet, i);
         }
 
         return resultArr;
     }
 
 
-    private int findQuietest(int[][] richer, int[] quiet, int index) {
+    private int findLoudest(int[][] richer, int[] quiet, int index) {
         if (resultArr[index] != -1) return resultArr[index];
         int result = quiet[index];
+        int person = index;
         for (int i = 0; i < richer.length; i++) {
             //find richer than index
             if (index == richer[i][1]) {
                 //get the result
-                int temp = findQuietest(richer, quiet, richer[i][0]);
-                if (result > temp) {
-                    result = temp;
+                int loudPerson = findLoudest(richer, quiet, richer[i][0]);
+                if (result > quiet[loudPerson]) {
+                    result = quiet[loudPerson];
+                    person = loudPerson;
                 }
             }
         }
-        resultArr[index] = result;
-        return result;
+        resultArr[index] = person;
+        return person;
     }
 }
