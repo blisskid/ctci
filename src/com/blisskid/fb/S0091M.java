3b30a853ef2e22489e26f19ad92388a2f5e9c060
blisskid <yuleirichard@163.com>
2020-11-17 23:06:12 +0800

    Merge branch 'master' of github.com:blisskid/ctci
    # Conflicts:
    #	src/com/blisskid/leetcode/Solution851.java

--- src/com/blisskid/fb/S0091M.java
+++ src/com/blisskid/fb/S0091M.java
@@ -3,65 +3,44 @@ package com.blisskid.fb;
 public class S0091M {
 
     public static void main(String[] args) {
-        System.out.println(new S0091M().numDecodings("2101"));
-    }
-
-    private String trimStartZero(String s){
-        StringBuilder sb=new StringBuilder();
-        int i=0;
-        while(i<s.length()&&s.charAt(i)=='0'){
-            i++;
-        }
-        while(i<s.length()){
-            sb.append(s.charAt(i));
-            i++;
-        }
-        return sb.toString();
-    }
-
-    private String trimOneZero(String s){
-        return s.replace("10","");
-    }
-
-    private boolean containsZero(String s){
-        s=s.replace("10","");
-        s=s.replace("20","");
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='0'){
-                return true;
-            }
-        }
-        return false;
+        System.out.println(new S0091M().numDecodings("226"));
     }
 
     public int numDecodings(String s) {
-        if(s==null)
+        char[] arr=s.toCharArray();
+        int len=arr.length;
+        if(len==0){
             return 0;
-        if(s.equals("0"))
-            return 0;
+        }
+        int[] f=new int[len];
+        if(arr[0]=='0')
+            f[0]=0;
+        else
+            f[0]=1;
 
-        String temp=trimStartZero(s);
-        if(temp.equals(""))
-            return 0;
-        //temp=trimOneZero(temp);
-        if(containsZero(temp)){
-            return 0;
-        }
-        char[] cArr=temp.toCharArray();
-        int start=0;
-        int end=1;
-        int count=0;
-        while(start<cArr.length&&end<cArr.length){
-            if(f(cArr,start,end)){
-                count++;
+        for(int i=1;i<len;i++){
+            if(arr[i]=='0'&&(arr[i-1]!='1'&&arr[i-1]!='2')){
+                return 0;
+            }else if(f2(arr,i-1,i)){
+                if(i==1){
+                    f[1]=2;
+                }else{
+                    f[i]=f[i-2]+f[i-1];
+                }
+            }else if(arr[i]=='0'&&(arr[i-1]=='1'||arr[i-1]=='2')){
+                if(i==1){
+                    f[1]=1;
+                }else{
+                    f[i]=f[i-2];
+                }
+            }else{
+                f[i]=f[i-1];
             }
-            start++;
-            end++;
         }
-        return count+1;
+        return f[len-1];
     }
 
-    private boolean f(char[] arr,int start,int end){
+    private boolean f2(char[] arr,int start,int end){
         StringBuilder sb=new StringBuilder();
         sb.append(arr[start]);
         sb.append(arr[end]);
@@ -72,35 +51,4 @@ public class S0091M {
             return false;
         }
     }
-/*
-    private int f2(char[] arr,int start,int end){
-        int size=end-start+1;
-        if(size==1)
-            return 1;
-        if(size==2){
-            return f1(arr,start,end);
-        }
-        int mid=(start+end) / 2;
-        int left=f(arr,start,mid);
-        int right=f(arr,mid+1,end);
-        return left+right+f1(arr,mid,mid+1);
-    }
-
-    private int f1(char[] arr,int start,int end){
-        StringBuilder sb=new StringBuilder();
-        sb.append(arr[start]);
-        sb.append(arr[end]);
-        Integer val=Integer.parseInt(sb.toString());
-        if(val==0){
-            return 0;
-        }else if(val>0&&val<10){
-            return 1;
-        }else if(val>=10&&val<=26){
-            return 2;
-        }else{
-            return 1;
-        }
-    }
-    */
-
 }
